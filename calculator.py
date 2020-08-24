f647a1050f5676c4decf41ac5c7cb57831b12fdb
Claire Matranga <cmatranga7@Claires-MacBook-Pro.local>
2020-08-24 13:08:36 -0400

    added function to check HDL rel to standards

--- calculator.py
+++ calculator.py
@@ -2,11 +2,21 @@ def HDL_driver():
         # get input
 	user_HDL = get_HDL()
         # check if HDL is normal
+	check_HDL(user_HDL)
         # Output
 
-def get_HDL()
+def get_HDL():
 	HDL = input("Enter your HDL: ")
 	return int(HDL)
+
+def check_HDL(HDL):
+	if HDL >= 60:
+		print("Your HDL is Normal")
+	elif HDL >= 40:
+		print("Your HDL is Blorderline Low")
+	else:
+		print("Your HDL is Low")
+	return
 
 def interface():
     print("My Program")
